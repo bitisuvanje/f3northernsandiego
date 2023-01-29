a84f8bbbedc7cab920f93436f735a97ed9446f31
Adam M <1672244+bitisuvanje@users.noreply.github.com>
2023-01-29 12:16:10 -0800

    fixing github.com links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/bitisuvanje/f3northernsandiego',
             label: 'GitHub',
             position: 'right',
           },
@@ -117,7 +117,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/bitisuvanje/f3northernsandiego',
               },
             ],
           },
